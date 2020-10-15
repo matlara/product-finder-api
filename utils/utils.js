f0f85eaf67416677a0e33a28f9925b3150fc15f3
Matias Lara <demoleithor.lara@gmail.com>
2020-10-15 14:58:14 +0000

    add | include utils with fucntions for: mergeArrays, palindrome and fiftyPercent

---
+++ utils/utils.js
@@ -0,0 +1,27 @@
+
+function mergeArrays(...arrays) {
+    let jointArray = []
+  
+    arrays.forEach(array => {
+        jointArray = [...jointArray, ...array]
+    });
+    return Array.from(new Set([...jointArray]))
+}
+
+function palindrome(str) {
+    if(str.length > 2) {
+        var re = /[\W_]/g;
+        var lowRegStr = String(str).toLowerCase().replace(re, '');
+        var reverseStr = lowRegStr.split('').reverse().join(''); 
+        return reverseStr === lowRegStr;
+    }
+    else {
+        return false;
+    }
+}
+
+function fiftyPercent(number) {
+    return number / 2;
+}
+
+module.exports = { mergeArrays, palindrome, fiftyPercent };
